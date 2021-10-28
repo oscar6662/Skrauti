054f6685db2182fc9bf178e02d7224909a420873
oscar6662 <oscar.berenguer6@gmail.com>
2021-10-28 12:20:42 +0000

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,36 @@
 import React from "react";
 
+import s from './footer.module.scss';
 
 export default function Footer() {
   return (
     <>
-      <div className="">
-
+      <div className={s.footer}>
+        <div className={s.footer__content}>
+          <div className={s.footer__left}>
+            <ul>
+              <li>
+                Kennitala: 650221-0520
+              </li>
+              <li>
+                0133-26-002338
+              </li>
+            </ul>
+          </div>
+          <div className={s.footer__right}>
+            <ul>
+              <li>
+                skrauti@skrauti.is
+              </li>
+              <li>
+                Facebook
+              </li>
+              <li>
+                Instagram
+              </li>
+            </ul>
+          </div>
+        </div>
 	    </div>
     </>
   );
