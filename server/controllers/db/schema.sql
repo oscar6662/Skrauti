868039cfe1889f5d9be0b19a49a5fea97188436f
oscar6662 <ohb5@hi.is>
2021-11-25 03:26:44 +0000

    fixd back

--- server/controllers/db/schema.sql
+++ server/controllers/db/schema.sql
@@ -5,12 +5,12 @@ CREATE TABLE IF NOT EXISTS users (
   username varchar(255) NOT NULL unique,
   email varchar(70) NOT NULL unique,
   password character varying(255) NOT NULL,
-  admin boolean default true,
+  admin boolean default true
 );
 
 DROP TABLE IF EXISTS candidates CASCADE;
 
 CREATE TABLE IF NOT EXISTS greinar (
   title varchar(1024) not null,
-  link varchar(1024) not null,
+  link varchar(1024) not null
 );
--- server/server.js
+++ server/server.js
@@ -4,14 +4,7 @@ import passport from 'passport';
 import path, {dirname} from 'path';
 import cors from 'cors';
 import { fileURLToPath } from 'url';
-import {
-  router as authRouter,
-  isAuthenticated,
-  requireAuthentication,
-} from './controllers/user/auth.js';
-import { router as candidateRouter } from './controllers/user/candidates.js';
-import { router as voteRouter } from './controllers/user/vote.js';
-import { userData, userIsAdmin } from './controllers/user/users.js';
+import { router as userRouter } from './controllers/user/users.js';
 
 dotenv.config();
 
@@ -23,23 +16,7 @@ app.use(express.static(path.join(__dirname, '/../client/build')));
 app.use(cors());
 app.use(express.json());
 app.use(passport.initialize());
-app.use(authRouter);
-app.use(candidateRouter);
-app.use(voteRouter);
-
-
-app.get('/api/authenticated', async (req, res) => {
-  return res.json(await isAuthenticated(req));
-});
-
-app.get('/api/user/isadmin', async (req, res) => {
-  return res.json(await userIsAdmin(req));
-});
-
-app.get('/api/user/name', requireAuthentication, async (req, res) => {
-  const data = await userData(req.cookies.token);
-  return res.json(data.data.personal.name_full);
-});
+app.use(userRouter);
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/../client/build/index.html'));
